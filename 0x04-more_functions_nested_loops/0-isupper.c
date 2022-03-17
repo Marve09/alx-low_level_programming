bc2eec2c0c009fc7e68c89fe860476bd1b06f529
Marve09 <Marvemarcel@gmail.com>
2022-03-17 22:51:54 +0100

    Nice

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,16 +5,11 @@
  *  Description:  a function that checks for uppercase character
  *  Return: always 0.
  */
-int main(void)
+int _isupper(int c)
 {
-	int c;
-		int _putchar(char c);
-
 	if (c >= 'A' && c <= 'Z')
 	return (1);
 	else
-	return (0);
-	printf("%d\n", c);
 
 	return (0);
 }
